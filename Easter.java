632d2dac6592ed30ed9e5bf89c8e8010ccd8b674
CurryQuantum <pedregosa.ernest@gmail.com>
2025-09-03 15:06:11 +0000

    first commit

---
+++ Easter.java
@@ -0,0 +1,15 @@
+public class Easter{
+    public static void main(String[] args) {      
+    }
+    double year = 2067.0;
+    double a;
+double b; 
+double c;
+double
+double
+double
+double
+    double a = year mod 19;
+    double b = year / 100;
+    double c = year mod 100; 
+}
